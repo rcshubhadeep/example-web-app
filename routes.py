00f3d57d80db0aee57fdd8a9d81a4a9a493785e6
David Rader <sa@adercon.com>
2016-09-09 18:00:43 -0400

    incremented version for flci integration testing, attempt #24

--- routes.py
+++ routes.py
@@ -1,9 +1,11 @@
 from app import app
 import os
 
-version = os.getenv('VERSION_STR', '0.1.27')
+version = os.getenv('VERSION_STR', '0.2.1')
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World! " + version
+    # @TODO we really need to do something in the db here too as its another
+    #       image and we want to show that they are hooked together.
